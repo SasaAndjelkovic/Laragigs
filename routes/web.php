2f79fd6372bf0bd8339d8ff3603e426ecbb46188
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-05-07 22:44:22 +0200

    routing and responses

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return response('<h1>Hello World</h1>')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function($id) {
+    return response('Post' . $id);
+})->where('id', '[0-9]+');
