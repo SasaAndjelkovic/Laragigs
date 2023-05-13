86505e960d0aa7e5528104691b0eddfedd99c01a
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-05-14 00:04:21 +0200

    API Routes

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,14 +19,4 @@ Route::get('/posts/{id}', function($id) {
 
 Route::get('/search', function(Request $request) {
     return $request->name . ' ' . $request->city;
-
-    // dd($request->name . ' ' . $request->city);
-
-    //dd($request);
-    // +query: Symfony\Component\HttpFoundation\InputBag {#51 ▼
-    //     #parameters: array:2 [▼
-    //       "name" => "Brad"
-    //       "city" => "Boston"
-    //     ]
-    //   }
 });
