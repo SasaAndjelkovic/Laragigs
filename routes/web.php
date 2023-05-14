7399d739f4e666caa973cc1da1d12bbbdd4eac57
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-05-14 20:42:06 +0200

    view basics and passing data

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,21 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+            'heading'=>'Latest Listings',
+            'listings'=>[  
+                [
+                    'id'=>1, 
+                    'title'=>'Listing One',
+                    'description'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+                ],
+                [
+                    'id'=>2, 
+                    'title'=>'Listing Two',
+                    'description'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+                ]
+            ]       
+        ]);
 });
 
 Route::get('/hello', function() {
