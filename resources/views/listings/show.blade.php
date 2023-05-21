8ca3b5dee21011e67c70481be5d5676017717783
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-05-21 15:40:17 +0200

    some bugs fixed

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,8 +1,9 @@
 <x-layout>
 
-<a href="/" class="inline-block text-black ml-4 mb-4">
-    <i class="fa-solid fa-arrow-left"></i> Back
-</a>
+    <a href="/" class="inline-block text-black ml-4 mb-4">
+        <i class="fa-solid fa-arrow-left"></i> Back
+    </a>
+    
     <div class="mx-4">
         <x-card class="p-24">
             <div
