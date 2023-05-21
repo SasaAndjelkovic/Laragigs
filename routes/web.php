1266854c27b600a0de99f61554f8f9238395ff77
SasaAndjelkovic <sandjelkovickg@gmail.com>
2023-05-21 15:28:00 +0200

    resource method naming

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
 
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
